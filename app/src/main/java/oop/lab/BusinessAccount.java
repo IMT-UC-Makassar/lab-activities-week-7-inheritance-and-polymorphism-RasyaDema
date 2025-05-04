f5262f52d410400cb294e832ed43e67918caf5be
Rasya Febrian Dema <rasyadema07@gmail.com>
2025-05-04 20:29:22 +0800

    fixing account polymorphisms

--- app/src/main/java/oop/lab/BusinessAccount.java
+++ app/src/main/java/oop/lab/BusinessAccount.java
@@ -1,6 +1,6 @@
 package oop.lab;
 
-public class BusinessAccount extends BankAccount implements LoanService {
+public class BusinessAccount extends BankAccount implements OnlineService, LoanService {
 
     public BusinessAccount(String accountNumber, String accountHolder, double balance) {
         super(accountNumber, accountHolder, balance);
@@ -21,5 +21,21 @@ public class BusinessAccount extends BankAccount implements LoanService {
     @Override
     public String checkLoanStatus() {
         return "Pending";
+    }
+
+    @Override
+    public void transferFunds(double amount, String toAccount) {
+        if (amount > 0 && balance >= amount) {
+            balance -= amount;
+            System.out.println("Transferred " + amount + " to account " + toAccount);
+        }
+    }
+
+    @Override
+    public void payBills(double amount) {
+        if (amount > 0 && balance >= amount) {
+            balance -= amount;
+            System.out.println("Paid bill of " + amount);
+        }
     }
 }
